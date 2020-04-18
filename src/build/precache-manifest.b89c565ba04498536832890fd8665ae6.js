1ed0c958e8ec638fb37f29c129e2f8ef8a08767e
vinko-design <vinko.k.design@gmail.com>
2020-04-18 14:56:57 +0100

    First Commit

---
+++ src/build/precache-manifest.b89c565ba04498536832890fd8665ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44ba827edaa96dbe6ba8c5bf1dee12b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b66755206203d2ba1d4f",
+    "url": "/static/css/main.84b4ca96.chunk.css"
+  },
+  {
+    "revision": "14fb5dd645820882a1a9",
+    "url": "/static/js/2.765f7c71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.765f7c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66755206203d2ba1d4f",
+    "url": "/static/js/main.f45d0469.chunk.js"
+  },
+  {
+    "revision": "539f2bdba5a3857705e9",
+    "url": "/static/js/runtime-main.4e8dae2e.js"
+  }
+]);
